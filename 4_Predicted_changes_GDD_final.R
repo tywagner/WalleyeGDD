a22e2d81cd86ed8e89878050ccb775f911e97db6
txw19 <txw19@psu.edu>
2016-04-19 11:05:44 -0400

    deleted plot

--- 4_Predicted_changes_GDD_final.R
+++ 4_Predicted_changes_GDD_final.R
@@ -76,6 +76,7 @@ colnames(ests_late) <- c('WBIC','mean_late','97.5%','2.5%')
 saveRDS(ests_early, file="early_estimates.rds")
 saveRDS(ests_late, file="late_estimates.rds")
 
+
 # testrun <- readRDS("late_estimates.rds")
 # head(testrun)
 
@@ -85,6 +86,11 @@ size.labels = 1
 size.text = 1
 
 probDiff <- mean_late - mean_early
+
+# Look at estimates for those with negative slopes
+probDiff[which(betas>0)]
+betas[21]
+betas[which(betas>0)]
 
 probDiffColor <- rep('blue',num)
 probDiffColor[probDiff < 0] <- 'red'
@@ -116,87 +122,87 @@ par(def.par)
 dev.off()
 
 
-
-#####################################################
-########### PLOT ####################################
-#####################################################
-res <- 6
-name_figure <- "gdd_change_plot2.jpg"
-jpeg(filename = name_figure, height = 500*res, width = 500*res, res=72*res)
-def.par <- par(no.readonly = TRUE)     # save default, for resetting...
-
-nf <- layout(matrix(c(1:2), nrow = 1, ncol=2,byrow=TRUE), widths = c(0.7, 0.3))
-layout.show(nf)
-par(mar = c(1, 1, 1, 0) + 0.2,oma=c(2,2,1,0))
-def.par <- par(no.readonly = TRUE)     #
-
-size.labels = 1
-size.text = 1
-
-x.label <- 'Probability of successful recruitment'
-y.label <- 'Lake number'
-
-# # Data from early model
-# rows <- 2060:2122
-# Plot.data <- out$summary[rows,]
-# plotting.region <- range(Plot.data[,c(3,7)])
 # 
-# # Data from ignore TP < 1 ug/L analysis
-# rows2 <- 2055:2117
-# Plot.data2 <- out2$summary[rows2,]
-# plotting.region2 <- range(Plot.data2[,c(3,7)])
-nlake <- 50 # num
-### axis label options
-spc <- 0.06
-lab <- 1:nlake
-cex <- 0.5
-adj <- 0
-const <- 0.5
-
-
-###
-plot(c(0, 1), c(1-const,nlake + const), 
-     axes=F, xlab='',ylab='',type='n')
-axis(side=1,cex.axis=size.text, mgp=c(0,0.5,0),tck= -0.01) #, at=xlab1, labels=round(xlab2,2)
-#axis(side=2,cex.axis=size.text,  mgp=c(0,0.5,0),tck= -0.01, las=1 ) # at=ylab1, labels=round(ylab2,2)
-axis(side=2,at=c(1:nlake),labels=F,tck= -0.01)
-
-text(par("usr")[1] - spc,1:nlake,srt = 0, adj =adj,
-     labels = lab, xpd = TRUE,cex=0.8)
-
-# 95% CIs for early analysis
-segments(x0=ci_early[2,1:nlake], x1=ci_early[1,1:nlake],
-         y0=1:nlake, y1=1:nlake, col='black',lwd=1)
-
-
-## Estiamtes from early model
-points(mean_early[1:nlake], 1:nlake, col='black',cex=0.5, pch=16)
-
-## Estimates from late model
-segments(x0=ci_late[2,1:nlake], x1=ci_late[1,1:nlake],
-         y0=1:nlake+const, y1=1:nlake+const, col='black',lwd=1,lty=2)
-points(mean_late[1:nlake], 1:nlake+const, col='firebrick4',cex=0.5, pch=15)
-# 95% CIs
-
-
-# Add x- and y-axis lables
-mtext(x.label, line = 0.8, side = 1, cex = size.text, outer=T, adj=0.35)
-mtext(y.label, line = 0.8, side = 2, cex = size.text, outer=T)
-
-# abline(h=0)
-box()
-
-### ADD legend
-par(mar = c(0, 0, 1, 2))
-plot(1:3, rnorm(3), pch = 1, lty = 1, ylim=c(-2,2), type = "n", axes = FALSE, ann = FALSE)
-legend(1, 1, c("Early GDD", "Late GDD"), pch = c(16,15), lty = c(1,2), cex=0.8, col=c('black','firebrick4'),
-       x.intersp = 0.5)
-###
-
-par(def.par)
-dev.off()
-
-
-
+# #####################################################
+# ########### PLOT ####################################
+# #####################################################
+# res <- 6
+# name_figure <- "gdd_change_plot2.jpg"
+# jpeg(filename = name_figure, height = 500*res, width = 500*res, res=72*res)
+# def.par <- par(no.readonly = TRUE)     # save default, for resetting...
+# 
+# nf <- layout(matrix(c(1:2), nrow = 1, ncol=2,byrow=TRUE), widths = c(0.7, 0.3))
+# layout.show(nf)
+# par(mar = c(1, 1, 1, 0) + 0.2,oma=c(2,2,1,0))
+# def.par <- par(no.readonly = TRUE)     #
+# 
+# size.labels = 1
+# size.text = 1
+# 
+# x.label <- 'Probability of successful recruitment'
+# y.label <- 'Lake number'
+# 
+# # # Data from early model
+# # rows <- 2060:2122
+# # Plot.data <- out$summary[rows,]
+# # plotting.region <- range(Plot.data[,c(3,7)])
+# # 
+# # # Data from ignore TP < 1 ug/L analysis
+# # rows2 <- 2055:2117
+# # Plot.data2 <- out2$summary[rows2,]
+# # plotting.region2 <- range(Plot.data2[,c(3,7)])
+# nlake <- 50 # num
+# ### axis label options
+# spc <- 0.06
+# lab <- 1:nlake
+# cex <- 0.5
+# adj <- 0
+# const <- 0.5
+# 
+# 
+# ###
+# plot(c(0, 1), c(1-const,nlake + const), 
+#      axes=F, xlab='',ylab='',type='n')
+# axis(side=1,cex.axis=size.text, mgp=c(0,0.5,0),tck= -0.01) #, at=xlab1, labels=round(xlab2,2)
+# #axis(side=2,cex.axis=size.text,  mgp=c(0,0.5,0),tck= -0.01, las=1 ) # at=ylab1, labels=round(ylab2,2)
+# axis(side=2,at=c(1:nlake),labels=F,tck= -0.01)
+# 
+# text(par("usr")[1] - spc,1:nlake,srt = 0, adj =adj,
+#      labels = lab, xpd = TRUE,cex=0.8)
+# 
+# # 95% CIs for early analysis
+# segments(x0=ci_early[2,1:nlake], x1=ci_early[1,1:nlake],
+#          y0=1:nlake, y1=1:nlake, col='black',lwd=1)
+# 
+# 
+# ## Estiamtes from early model
+# points(mean_early[1:nlake], 1:nlake, col='black',cex=0.5, pch=16)
+# 
+# ## Estimates from late model
+# segments(x0=ci_late[2,1:nlake], x1=ci_late[1,1:nlake],
+#          y0=1:nlake+const, y1=1:nlake+const, col='black',lwd=1,lty=2)
+# points(mean_late[1:nlake], 1:nlake+const, col='firebrick4',cex=0.5, pch=15)
+# # 95% CIs
+# 
+# 
+# # Add x- and y-axis lables
+# mtext(x.label, line = 0.8, side = 1, cex = size.text, outer=T, adj=0.35)
+# mtext(y.label, line = 0.8, side = 2, cex = size.text, outer=T)
+# 
+# # abline(h=0)
+# box()
+# 
+# ### ADD legend
+# par(mar = c(0, 0, 1, 2))
+# plot(1:3, rnorm(3), pch = 1, lty = 1, ylim=c(-2,2), type = "n", axes = FALSE, ann = FALSE)
+# legend(1, 1, c("Early GDD", "Late GDD"), pch = c(16,15), lty = c(1,2), cex=0.8, col=c('black','firebrick4'),
+#        x.intersp = 0.5)
+# ###
+# 
+# par(def.par)
+# dev.off()
+# 
+# 
+# 
 
 
